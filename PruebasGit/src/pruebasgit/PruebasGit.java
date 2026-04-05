9a417c7568d16473914095957125a09d562cd8d2
poker251212-design <poker251212@gmail.com>
2026-04-05 14:51:46 -0500

    Add files via upload

---
+++ PruebasGit/src/pruebasgit/PruebasGit.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebasgit;
+
+/**
+ *
+ * @author ISMAEL
+ */
+public class PruebasGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Primer Commit: Proyecto PruebasGit de Ismael - UPN");
+        System.out.println("Mensaje de prueba para cambio en ramas");
+    }
+    
+}
